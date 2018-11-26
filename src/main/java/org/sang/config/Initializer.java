39bbcfd88e55f077316379d72a2769207af56508
hyjlmj <hyjlmj@163.com>
2018-11-27 05:29:04 +0800

    init

--- src/main/java/org/sang/config/Initializer.java
+++
@@ -1,9 +0,0 @@
-package org.sang.config;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class Initializer extends AbstractHttpSessionApplicationInitializer {
-
-
-
-}
